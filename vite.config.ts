46d2cdb6fdec6ed7980caefc1a6313ac98200d73
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2023-08-09 12:58:05 -0600

    updated vite.config with dynamicImport plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), dynamicImport()]
 }
 
 export default config
