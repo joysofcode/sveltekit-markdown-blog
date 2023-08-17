3bdb58fe25668b867549a15c8637c736ba295606
panosmakris <ppcpanos.work@gmail.com>
2023-08-17 22:58:08 +0300

    c

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,6 @@
 
 
 
-
 <div class="layout">
 	<Header />
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static'
+
+import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 import { mdsvex, escapeSvelte } from 'mdsvex'
@@ -28,17 +29,15 @@ const mdsvexOptions = {
 const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-	kit: {
+	kit:{
 		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
-        })
-	}
-}
+		  // See below for an explanation of these options
+		  routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		  }
+		})
+	  }
+	};
 
 export default config
