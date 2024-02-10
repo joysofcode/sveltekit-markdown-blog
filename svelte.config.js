b9652558179f500af94a182e229fe2a5e3cc8cdd
mattcroat <mattcroat.dev@gmail.com>
2024-02-10 16:17:33 +0100

    fix: Update Shiki and Svelte plugin for Vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 import { mdsvex, escapeSvelte } from 'mdsvex'
-import shiki from 'shiki'
+import {getHighlighter } from 'shiki'
 import remarkUnwrapImages from 'remark-unwrap-images'
 import remarkToc from 'remark-toc'
 import rehypeSlug from 'rehype-slug'
@@ -15,9 +15,13 @@ const mdsvexOptions = {
 	},
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
-			const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
-			return `{@html \`${html}\` }`
+      const highlighter = await getHighlighter({
+        themes: ['poimandres'],
+        langs: ['javascript', 'typescript']
+      });
+      await highlighter.loadLanguage('javascript', 'typescript');
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+      return `{@html \`${html}\` }`;
 		}
 	},
 	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
