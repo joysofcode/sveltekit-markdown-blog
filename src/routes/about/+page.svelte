71e9fcea53ee24c343a7768044978394141d00b5
Thomas Pritchard <tompritchard@Thomass-MacBook-Air.local>
2024-05-18 10:08:30 +1000

    Update config.ts and about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,5 @@
-<h1>About</h1>
-<p>I like long walks on the beach.</p>
+<h1>About Me</h1>
+<p>Hi, I am Tom Pritchard! A Sydney based music producer, DJ, digital technology enthusiast, digital transformation consultant, amateur sim racer and passionate people person! <br/>  <br/>
+I decided to start this blog as a means to express insights and perspectives on the things that are important to me. I plan to utilise this domain to express my views and document my journey through life. Feel free to drop me a line <a href="/contact">right here!</a> <br /><br>
+You can also subscribe to my posts via RSS or sign up to be notified via email... Once i get around to setting up a database and web form! 🫣
+</p>
