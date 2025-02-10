98bbf52d330d2aeb1b86709b7c0d2c0009961393
Petar Radosevic <petar@petar.dev>
2025-02-10 10:56:42 +0100

    chore: add missing types that fix eslint warnings

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
+import type { LayoutLoadEvent } from './$types'
+
 export const prerender = true
 
-export async function load({ url }) {
+export async function load({ url }: LayoutLoadEvent) {
 	return {
 		url: url.pathname
 	}
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit'
+import type { PageLoadEvent } from './$types'
 
-export async function load({ params }) {
+export async function load({ params }: PageLoadEvent) {
 	try {
 		const post = await import(`../../posts/${params.slug}.md`)
 
